a47527bc283b120e5cd144665613602b16ee428f
ekaitzAguilar <ekaitz.aguilarbi@elorrieta-errekamari.com>
2026-05-02 21:43:33 +0200

    Incluir archivos de environment en el repo (anon key es pública)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://tuefevyddkqvkcbmfxug.supabase.co',
+  supabaseAnonKey: 'sb_publishable_EZ7gYXRFJgZLrZjKWZJPoQ_PnZoyK7A',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://tuefevyddkqvkcbmfxug.supabase.co',
+  supabaseAnonKey: 'sb_publishable_EZ7gYXRFJgZLrZjKWZJPoQ_PnZoyK7A',
+};
