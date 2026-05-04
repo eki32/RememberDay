77a44a91de4426688397baa68714d6e0bba4dae5
ekaitzAguilar <ekaitz.aguilarbi@elorrieta-errekamari.com>
2026-05-05 00:16:54 +0200

    quitamos entorno de prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1ZWZldnlkZGtxdmtjYm1meHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NjA3NzQsImV4cCI6MjA5MzIzNjc3NH0.6nFOx_SLdz6qYnR8Rjp-oPKoJ5-sByf8xWep-CUUmNo',
 
   // Añade dentro del objeto environment:
-stripePublishableKey: 'pk_test_51TTQGqHa6VVygpaOgQbst7IyQbp6XwLbY3gtcM89arBnKMzwaRABk9PBim4mSL4niUvfUZoUTZBWEG1kJpVawRo000Cf9IzJ6D',
-stripePriceEventoUnico: 'price_1TTQNgHa6VVygpaOdbi21RWR',
-stripePricePro: 'price_1TTQPKHa6VVygpaOXHOpglIn',
+stripePublishableKey: 'pk_live_51TTQGfHHS5HV3CjKO22EWh3mlykVcljgWtMtjCG7Tj1jetDXLy8xOb2dkfK50ORGuUQH1POIPqPAFSwFStnqotza00qEXv1h7n',
+stripePriceEventoUnico: 'prod_USL30cjdNud4Qf',
+stripePricePro: 'price_1TTUbZHHS5HV3CjKtAPueSdo',
 };
