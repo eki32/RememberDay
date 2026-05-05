08396b78532fc9a95c1e53fd0dffd9cf07815d00
ekaitzAguilar <ekaitz.aguilarbi@elorrieta-errekamari.com>
2026-05-05 22:25:10 +0200

    Nueva cuenta Stripe como autónomo - Live mode activo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1ZWZldnlkZGtxdmtjYm1meHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NjA3NzQsImV4cCI6MjA5MzIzNjc3NH0.6nFOx_SLdz6qYnR8Rjp-oPKoJ5-sByf8xWep-CUUmNo',
 
   // Añade dentro del objeto environment:
-stripePublishableKey: 'pk_live_51TTQGfHHS5HV3CjKO22EWh3mlykVcljgWtMtjCG7Tj1jetDXLy8xOb2dkfK50ORGuUQH1POIPqPAFSwFStnqotza00qEXv1h7n',
-stripePriceEventoUnico: 'price_1TTUbXHHS5HV3CjKwqge4xX9',
-stripePricePro: 'price_1TTUbZHHS5HV3CjKtAPueSdo'
+stripePublishableKey: 'pk_live_51TTpA85ZniJIZQy7a3372Rinb0cU1g9dV0RZ3BWvhwFGzDi051K5zQcV385yl8FoE5eQ02v5xsTJ7vPv5ItyzT9700jwYbgbHo',
+stripePriceEventoUnico: 'price_1TTpQp5ZniJIZQy7hxi7NOkd',
+stripePricePro: 'price_1TTpRY5ZniJIZQy70fy9D1TN'
 };
